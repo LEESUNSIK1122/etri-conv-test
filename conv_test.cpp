1bdf7142c87501b3fa7dea2afe678491d76989a0
LEESUNSIK1122 <soonsick2@nate.com>
2019-04-03 10:51:36 +0900

    change filter

--- conv_test.cpp
+++ conv_test.cpp
@@ -11,7 +11,7 @@ int main ( int argc, char** argv )
     if( !src.data )  { return -1; }
 
     // Make filter
-    kernel = (cv::Mat_<float>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1);
+    kernel = (cv::Mat_<float>(3, 3) << 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11);
     // Run 2D filter
     cv::filter2D(src, dst, -1 , kernel, cv::Point( -1, -1 ), 0, cv::BORDER_DEFAULT );
 
