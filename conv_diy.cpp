5448770d32d0ded94d18a4fd90387c221aa8afc3
LEESUNSIK1122 <soonsick2@nate.com>
2019-04-04 15:19:44 +0900

    convolution

--- conv_diy.cpp
+++ conv_diy.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-
+/*
 int your_conv( cv::Mat src,
                cv::Mat dst,
                cv::Mat kernel,
@@ -27,19 +27,6 @@ dst_height = src_height;
 dst_width = src_width;
 
 
-dst = cv::Mat_<float>(1000, 1000, 3);
-
-for(i = 0; i<1000; i= i+1)
-{
-	for(j = 0; j<1000; j= j+1)
-	{
-     //   std::cout << src.at<cv::Vec3b>(0,0)[0] << std::endl;
-     //   std::cout << dst.at<cv::vec3b>(i,j)[0] << std::endl;
-	//	dst.at<cv::Vec3b>(i,j)[0] = 100;
-	//	dst.at<cv::Vec3b>(i,j)[1] = 100;
-	//	dst.at<cv::Vec3b>(i,j)[2] = 100;
-	}
-}
 
 
     // if success
@@ -48,7 +35,7 @@ for(i = 0; i<1000; i= i+1)
     // if fail - in the case of wrong parameters...
     // return -1
 }
-
+*/
 int main ( int argc, char** argv )
 {
     if (argc < 2) {
@@ -73,11 +60,37 @@ int main ( int argc, char** argv )
     //cv::filter2D(src, dst, -1 , kernel, cv::Point( -1, -1 ), 0, cv::BORDER_DEFAULT );
 
     // ---------------------------
-    your_conv(src,dst,kernel,1,0);
+   // your_conv(src,dst,kernel,1,0);
     // ---------------------------
 
+dst = cv::imread( argv[1] );
+int i,j,k,s;
+int sum[3]={0};
+
+for(i = 0; i<dst.rows-2; i= i+1)
+{
+	for(j = 0; j<dst.cols-2; j= j+1)
+	{
+		sum[0]=0;
+		sum[1]=0;
+		sum[2]=0;
+		for(k=0; k<3; k++)
+		{
+			for(s = 0; s<3; s++)
+			{
+			sum[0] += src.at<cv::Vec3b>(i+k,j+s)[0] * kernel.at<cv::Vec3b>(k,s)[0];
+			sum[1] += src.at<cv::Vec3b>(i+k,j+s)[1] * kernel.at<cv::Vec3b>(k,s)[1];
+			sum[2] += src.at<cv::Vec3b>(i+k,j+s)[2] * kernel.at<cv::Vec3b>(k,s)[2];
+			}		
+		}		
+		dst.at<cv::Vec3b>(i,j)[0] =sum[0];
+		dst.at<cv::Vec3b>(i,j)[1] =sum[1];
+		dst.at<cv::Vec3b>(i,j)[2] =sum[2];
+	}
+}
+
     cv::namedWindow( "filter2D Demo", cv::WINDOW_AUTOSIZE );
-    cv::imshow( "filter2D Demo", kernel );
+    cv::imshow( "filter2D Demo", dst );
 
     cv::waitKey(0);
     return 0;
